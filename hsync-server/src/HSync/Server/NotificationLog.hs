0e5a0afaeb6e5e522349d1f5467fa990473580b2
Frank Staals <frank@Hina.local>
2013-08-23 20:55:08 +0200

    cleaned up a bit

--- hsync-server/src/HSync/Server/NotificationLog.hs
+++ hsync-server/src/HSync/Server/NotificationLog.hs
@@ -15,46 +15,14 @@ import qualified Data.ByteString.Char8 as B
 import Data.Time.Calendar
 import Data.Time.Clock
 
--- writeNotificationLog :: Handler ()
--- writeNotificationLog = do
---                          ns  <- notifications
---                          dir <- extraNotificationLogDir <$> getExtra
---                          runResourceT $ notificationsByDate dir ns
+writeNotificationLog :: Handler ()
+writeNotificationLog = do
+                         ns  <- notifications
+                         dir <- extraNotificationLogDir <$> getExtra
+                         runResourceT $ notificationsByDate dir ns
 
-
-
-nextDay (DateTime (UTCTime d t)) = DateTime $ UTCTime (addDays 1 d) t
-
-generateNots = do
-  now <- currentTime
-  let days = take 10 . iterate nextDay $ now
-  return . map noti . concatMap (replicate 3) $ days
-      where
-        path = Path "" []
-        noti = (Notification (FileAdded path) "")
-
--- -- notificationFileSink :: MonadIO m => Sink Notification m ()
--- notificationFileSink dir s = do
---                            (rs,_) <- s $$+ byDate =$
-
-
-
-testNots = do
-  n <- generateNots
-  let s = C.sourceList n
-  runResourceT $ notificationsByDate "/tmp" s
-
-
-
-
--- splitC   :: (MonadIO m, Show a, Num a) => (a -> Bool) -> Sink a m ()
-
-
-
--- notificationsByDate :: --Monad m => Conduit Notification m Notification
-
--- notificationsByDate       :: (MonadResource m, MonadIO m) => FilePath ->
---                              Source m Notification -> Sink Notification m ()
+notificationsByDate       :: MonadResource m => FilePath -> Source m Notification ->
+                             m ()
 notificationsByDate dir s = (s $$+ sink) >>= f . fst
     where
       sink = takeWhileByDate =$ simpleNotificationSinkByDate dir
@@ -85,8 +53,6 @@ simpleNotificationSinkByDate'       :: (MonadResource m, MonadIO m) =>
 simpleNotificationSinkByDate' dir n = let ds = show . day . timestamp $ n
                                           fp = dir ++ "/" ++ ds           in
                                      simpleNotificationFileSink fp
-
-
 
 -- | A Sink that simply logs all notifcations to the given file
 simpleNotificationFileSink   :: (MonadResource m, MonadIO m) =>
@@ -124,6 +90,30 @@ takeWhileC p = await >>= maybe (return ())
 --------------------------------------------------------------------------------
 -- | Testing stuff
 
+
+nextDay (DateTime (UTCTime d t)) = DateTime $ UTCTime (addDays 1 d) t
+
+generateNots = do
+  now <- currentTime
+  let days = take 10 . iterate nextDay $ now
+  return . map noti . concatMap (replicate 3) $ days
+      where
+        path = Path "" []
+        noti = (Notification (FileAdded path) "")
+
+-- -- notificationFileSink :: MonadIO m => Sink Notification m ()
+-- notificationFileSink dir s = do
+--                            (rs,_) <- s $$+ byDate =$
+
+
+
+testNots = do
+  n <- generateNots
+  let s = C.sourceList n
+  runResourceT $ notificationsByDate "/tmp" s
+
+
+
 eqToFrst :: (Eq a, Monad m) => Conduit a m a
 eqToFrst = conduitFromFirst (==)
 
@@ -148,20 +138,6 @@ source = C.sourceList [0,0,0,1,1,1,2,2,3,3,3]
 sink :: (Show a, MonadIO m) => Sink a m ()
 sink = await >>= maybe (return ()) (\x -> (liftIO $ print x) >> sink)
 
--- takeWhileC   :: Monad m => (a -> Bool) -> ConduitM a a m (Source m a)
--- takeWhileC p = await >>= maybe (return C.sourceNull)
---                                (\x -> if p x then return $ yield x >> takeWhileC p
---                                              else return $ leftover x)
-
-
-
-
--- splitC' p s = do
---              (next,_) <- s $$++ (takeWhileC p =$ sink)
---              mh       <- C.head next
---              liftIO $ print $ "PART DONE"
---              splitC' p (C.drop 1 next)
-
 test = runResourceT $ source $$+ (takeWhileC (/=0) =$ sink)
 
 test2 = runResourceT $ splitC (/=0) source
@@ -174,12 +150,3 @@ splitC p s = do
              (s',_)   <- unwrapResumable rs'
              liftIO $ print $ "PART DONE: " ++ show mh
              splitC p s'
-
-
--- do
--- runResourceT $ do
--- let s = sourceFile "input.txt" -- $= B.lines
--- (next, head) <- s $$+ (B.takeWhile (/= nl) >+> L.consume) -- (B.lines >+> L.take 1)
--- liftIO $ print head
--- rest <- next $$+- L.consume
--- liftIO $ print rest
