be16aafc4faade36efd35d1648e66b551fe3709d
Frank Staals <frank@Hina.local>
2013-10-04 20:48:36 +0200

    working on getting the remote part of the syncing to work

--- hsync-client/src/HSync/Client/Actions.hs
+++ hsync-client/src/HSync/Client/Actions.hs
@@ -66,6 +66,8 @@ import qualified Data.Text                  as T
 
 
 
+import Debug.Trace
+
 --------------------------------------------------------------------------------
 
 login :: ( MonadResource m, Failure HttpException m
@@ -98,6 +100,7 @@ getRemoteTree   :: ( MonadResource m, MonadThrow m
                    , MonadBaseControl IO m, Failure HttpException m) =>
                    Path -> ActionT m (FSTree DateTime)
 getRemoteTree p = do
+                    liftIO $ print $ "path: " ++ show p
                     resp <- runGetRoute $ TreeR p
                     lift $ responseBody resp C.$$+- parseFromJSONSink
 
@@ -122,16 +125,21 @@ getFile p = do
   when (status == ok200) $ lift (responseBody resp C.$$+- sinkFile lp)
 
 
-downloadFile :: ( MonadResource m, Failure HttpException m
-                , MonadBaseControl IO m) =>
-               (FileIdent, SubPath) -> ActionT m ()
-downloadFile (_,sp) = toRemotePath' sp >>= getFile
+downloadFile                 :: ( MonadResource m, Failure HttpException m
+                                , MonadBaseControl IO m) =>
+                               (FileIdent, SubPath) -> ActionT m ()
+downloadFile (fi,sp)
+             | traceShow (fi,sp) False = undefined
+             | isFile fi = toRemotePath' sp >>= getFile
+             | otherwise = return ()
 
 
 -- downloadFile :: ( MonadResource m, Failure HttpException m
 --                 , MonadBaseControl IO m) =>
 --                (Change FileIdent, SubPath) -> ActionT m ()
-patchFile = undefined
+patchFile _ = return ()
+
+patchRemote _ = return ()
 
 --------------------------------------------------------------------------------
 
@@ -159,8 +167,9 @@ putFile' fp fi p = do
 
 uploadFile :: ( MonadResource m, Failure HttpException m
               , MonadIO m, MonadBaseControl IO m) =>
-             (FileIdent, SubPath) -> ActionT m ()
-uploadFile (fi,sp) = do
+             (FSChange, SubPath) -> ActionT m ()
+uploadFile (c,sp) = let fi = remote c in
+                    do
                        rp <- toRemotePath' sp
                        lp <- toLocalPath'  sp
                        putFile' lp fi rp
@@ -170,6 +179,10 @@ uploadFile (fi,sp) = do
 
 deleteFile :: MonadIO m => (FileIdent, SubPath) -> ActionT m ()
 deleteFile _ = return () -- TODO, fix this
+
+
+deleteRemote _ = return ()
+
 
 --------------------------------------------------------------------------------
 
@@ -186,14 +199,21 @@ syncTree               :: ( MonadResource m, Failure HttpException m
                           , MonadIO m, MonadBaseControl IO m) =>
                           Path -> ActionT m ()
 syncTree p@(Path _ sp) = do
-  Just oldRemote <- subTree sp <$> remoteTree -- TODO: fix the just
+  oldRemote <- subTree sp <$> remoteTree -- TODO: fix the just
+  liftIO $ print "getting remote tree"
   newRemote <- getRemoteTree p
+  liftIO $ print "reading local tree"
   newLocal  <- readFSTree =<< toLocalPath p
+  liftIO $ print "handling changes"
   handleChanges $ detectFSChanges oldRemote newRemote newLocal
 
 
 runTree   :: (Monad m, Functor m) => ((l,SubPath) -> m b) -> FSTree l -> m (FSTree b)
-runTree f = runBottomUp f . labelWithSubPaths
+runTree f = runBottomUp f . fmap dropRootName . labelWithSubPaths
+    where
+      dropRootName (l,(_:sp)) = (l,sp)
+      -- Drop the name of the root directory from our FSTree (since in the remoteTree
+      -- this name of the user                          )
 
 
 handleChanges         :: ( MonadResource m, Failure HttpException m
@@ -201,7 +221,15 @@ handleChanges         :: ( MonadResource m, Failure HttpException m
                         FSChanges -> ActionT m ()
 handleChanges changes = do
   runTree handleConflict . toHandleConflicts $ changes
+  liftIO $ print "Deleting stuff"
   runTree deleteFile     . toDeleteLocal     $ changes
+  liftIO $ print "Downloading stuff:"
+  liftIO $ print $ toDownload changes
   runTree downloadFile   . toDownload        $ changes
+  liftIO $ print "patching stuff "
   runTree patchFile      . toPatchLocal      $ changes
+
+  runTree deleteRemote   . toDeleteRemote    $ changes
+  runTree uploadFile     . toUpload          $ changes
+  runTree patchRemote    . toPatchRemote     $ changes
   return ()
