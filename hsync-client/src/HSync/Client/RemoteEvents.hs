f319580ef78f2fe1523ce47eb3e8e0faa7afc38a
Frank Staals <frank@Hina.local>
2013-11-24 14:51:01 +0100

    remoteEvents

--- hsync-client/src/HSync/Client/RemoteEvents.hs
+++ hsync-client/src/HSync/Client/RemoteEvents.hs
@@ -5,27 +5,48 @@ import HSync.Client.ActionT
 import HSync.Client.Actions
 
 
-handleNotification                  :: ( MonadResource m, Failure HttpException m
-                                       , MonadBaseControl IO m) =>
-                    Notification -> ActionT m ()
-handleNotification (Notification e)
 
 
 
 
+
+
+
+
+
+
+handleNotification                       :: ( MonadResource m, Failure HttpException m
+                                            , MonadBaseControl IO m) =>
+                                            Notification -> ActionT m ()
+handleNotification (Notification e ci t) = protect (noConflict e t)
+                                                   (handleEvent e)
+                                                   (handleIncomingConflict n)
+
+
+
+noConflict e t =
+
+
+conflictsLocal         :: Path -> Maybe FileIdent -> DateTime -> ActionT m Bool
+conflictsLocal p mfi t = return False -- TODO!!
 
 
 handleEvent (FileAdded p)           = getFile p
-handleEvent (FileRemoved p fi)      = deleteFile (fi,subPath p)
-handleEvent (FileUpdated p fi)      =
+handleEvent (FileRemoved p fi)      = deleteLocalFile fi p
+handleEvent (FileUpdated p fi)      = return ()
 handleEvent (FileMoved f fi t)      = error "handleEvent: not implemented yet"
 
 handleEvent (DirectoryAdded p)      = createLocalDirectory p
 
-handleEvent (DirectoryRemoved p fi) =
+handleEvent (DirectoryRemoved p fi) = return ()
 handleEvent (DirectoryUpdated p)    = error "handleEvent: not implemented yet"
 handleEvent (DirectoryMoved f t)    = error "handleEvent: not implemented yet"
 
 
 
-createLocalDirectory p = return ()
+
+handleIncomingConflict _ = return ()
+
+
+
+-- deleteLocalFile fi fp = protect
