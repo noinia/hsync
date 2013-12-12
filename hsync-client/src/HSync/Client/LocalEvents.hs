34e9bbb607e17e5fd6c02d71c7086e6596e39b63
Frank Staals <frank@Hina.local>
2013-12-12 14:05:28 +0100

    Started with local events :)

---
+++ hsync-client/src/HSync/Client/LocalEvents.hs
@@ -0,0 +1,24 @@
+module HSync.Client.LocalEvents where
+
+import HSync.Client.Import
+
+import HSync.Client.ActionT
+import HSync.Client.Actions
+import HSync.Client.Sync(Sync)
+
+import System.FSNotify
+
+import qualified System.FSNotify as FSN
+
+-- | should we use the time?
+handleEvent                 :: ( MonadResource m, Failure HttpException m
+                               , MonadIO m, MonadBaseControl IO m) =>
+                               FSN.Event -> ActionT m ()
+handleEvent (Added fp _)    = putFileOrDir fp
+handleEvent (Modified fp _) = return ()  -- TODO: We need the OLD fileident here
+handleEvent (Removed fp _)  = return ()  --       same here
+
+
+-- syncUpstream :: ( MonadResource m, Failure HttpException m
+--                       , MonadBaseControl IO m) =>
+--                       DateTime -> Path -> ActionT m ()
