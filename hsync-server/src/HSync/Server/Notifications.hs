8a46de5040affa006ef7d60a68618cd446637131
Frank Staals <frank@Hina.local>
2014-01-04 21:56:35 +0100

    A separate module for the notifications

---
+++ hsync-server/src/HSync/Server/Notifications.hs
@@ -0,0 +1,61 @@
+module HSync.Server.Notifications where
+
+import Prelude
+
+import Control.Applicative((<$>))
+
+import Control.Concurrent.STM.TChan
+import Control.Concurrent.STM(atomically)
+import Control.Monad.IO.Class(MonadIO(..))
+
+import Data.Conduit
+
+import HSync.Common.Notification
+
+import HSync.Server.AcidSync(AcidSync, notificationUpdate)
+import HSync.Server.Foundation
+
+import Yesod
+
+--------------------------------------------------------------------------------
+-- | Storing Notifications
+
+-- | Log the given notification.
+logNotification   :: Notification -> Handler ()
+logNotification n = do
+                    c <- notificationChan <$> getYesod
+                    lift $ atomically (writeTChan c n)
+
+
+-- | Consume notifications by storing them in our FSState tree
+notificationSink     :: MonadIO m => HSyncServer -> Sink Notification m ()
+notificationSink hss = let acid = acidSync hss in
+                       awaitForever $ lift . notificationUpdate acid
+
+--------------------------------------------------------------------------------
+-- | Obtaining Notifications
+
+-- | Get a stream of notifications in the Handler monad
+notifications :: Handler (Source Handler Notification)
+notifications = getYesod >>= notifications'
+
+
+-- | Get a stream of notifications
+notifications' :: (Functor m, MonadIO m) => HSyncServer -> m (Source m Notification)
+notifications' = fmap chanToSource . dupChan . notificationChan
+  where
+    dupChan c = liftIO $ atomically (dupTChan c)
+
+
+chanToSource   :: MonadIO m => TChan a -> Source m a
+chanToSource c = do
+                   x <- liftIO $ atomically (readTChan c)
+                   yield x
+                   chanToSource c
+
+--------------------------------------------------------------------------------
+
+-- | Keep track of all notifications in the given HSyncServer. I.e. log all
+--  notifications produced into our FSState tree
+storeNotifications     :: (Functor m, MonadIO m) => HSyncServer -> m ()
+storeNotifications hss = notifications' hss >>= ($$ notificationSink hss)
