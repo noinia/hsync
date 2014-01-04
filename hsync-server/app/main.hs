227b5591633fe9bebab44aba4d982515d49d2192
Frank Staals <frank@Hina.local>
2014-01-04 21:57:20 +0100

    Running the server with Acid

--- hsync-server/app/main.hs
+++ hsync-server/app/main.hs
@@ -1,8 +1,12 @@
-import Prelude                 (IO)
-import Yesod.Default.Config    (fromArgs)
+import Prelude                 (IO, return, ($))
+import Yesod.Default.Config    (fromArgs,appExtra)
 import Yesod.Default.Main      (defaultMain)
 import HSync.Server.Settings   (parseExtra)
 import HSync.Server.Application(makeApplication)
+import HSync.Server.AcidSync   (withAcidSync)
 
 main :: IO ()
-main = defaultMain (fromArgs parseExtra) makeApplication
+main = do
+         config <- fromArgs parseExtra
+         withAcidSync (appExtra config) $ \acidSync ->
+           defaultMain (return config) (makeApplication acidSync)
