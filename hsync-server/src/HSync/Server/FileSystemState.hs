7f8920b76b017df93881f9b8b9c0f2369943a2ca
Frank Staals <frank@Hina.local>
2014-05-07 23:12:39 +0200

    Working on Producing a Notification source from the stored FSState

--- hsync-server/src/HSync/Server/FileSystemState.hs
+++ hsync-server/src/HSync/Server/FileSystemState.hs
@@ -20,6 +20,9 @@ import Control.Monad.IO.Class(MonadIO(..))
 
 import Data.Aeson.TH
 import Data.Data(Data, Typeable)
+import Data.Sequence(Seq)
+
+
 
 import Data.SafeCopy(base, deriveSafeCopy)
 
@@ -35,6 +38,8 @@ import HSync.Common.Notification( Event(..)
                                 )
 import HSync.Common.Types
 
+import qualified Data.Foldable             as F
+import qualified Data.Sequence             as S
 import qualified HSync.Common.Notification as N
 
 --------------------------------------------------------------------------------
@@ -53,9 +58,15 @@ instance AsDateTime FileLabel where
   toDateTime = timestamp
 
 
-
+-- | Extract a file label from a Notification
 fromNotification                                     :: Notification -> FileLabel
 fromNotification (Notification (Event ek _ fi) ci t) = FileLabel ek ci t (Just fi)
+
+
+-- | Given a file label and a path. Reconstruct the corresponding notification
+toNotification                                 :: FileLabel -> Path -> Notification
+toNotification (FileLabel ek ci t (Just fi)) p = Notification (Event ek p fi) ci t
+toNotification _                             _ = error "toNotification: No FileIdent"
 
 --------------------------------------------------------------------------------
 
@@ -83,6 +94,8 @@ withTimedFSTree f = FSState . f . fsStateTree
 
 --------------------------------------------------------------------------------
 
+-- | Update the FileSystem State by the incoming notification. I.e. update,
+-- add, or delete the indicated item in the tree.
 updateNotification   :: Notification -> FSState -> FSState
 updateNotification n = withTimedFSTree (FSTree . f p . unTree)
   where
@@ -104,3 +117,60 @@ updateNotification n = withTimedFSTree (FSTree . f p . unTree)
     p'          = affectedPath . event $ n
     (p'',fName) = andLast $ subPath p'
     p           = (unUI $ owner p') : p''
+
+
+
+
+type Directory' = Directory (Max DateTime) (FileData FileLabel)
+
+type ReversedSubPath = SubPath
+
+
+notificationsAsOf     :: DateTime -> Path -> Source m Notification
+notificationsAsOf dt p = undefined -- TODO
+
+
+
+-- | Get all notifications for files stored in the filesystem rooted at this
+-- directory, in increasing order of time at which they happened.
+-- The clientId are the userId to use in the path, and ReverseSubPath the subpath
+-- pointing to the file, stored in reverse order.
+getNotificationsAsOf          :: DateTime -> UserIdent -> ReversedSubPath
+                              -> Directory' -> [Notification]
+getNotificationsAsOf dt u rp dir
+  | measurement dir < Max dt  = [] -- All changes in this directory occured before dt.
+  | otherwise                 = myChanges `merge` fileChanges `merge` recursiveChanges
+
+    where
+      -- localDirChanges  = changes . subDirectories $ dir
+      myChanges        = changes . S.singleton $ dir
+      fileChanges      = changes . files       $ dir
+      recursiveChanges = foldr1 merge [ getNotificationsAsOf dt u (name d : rp) d
+                                      | d <- F.toList $ subDirectories dir
+                                      ]
+
+      changes :: IsFileOrDirectory t
+              => Seq (t m (FileData FileLabel))
+              -> [Notification]
+      changes = F.toList
+              . S.unstableSort
+              . fmap toNotification'
+              . S.filter (\x -> timestamp' x >= dt)
+
+      timestamp' :: IsFileOrDirectory t => t m (FileData FileLabel) -> DateTime
+      timestamp' = timestamp . extractData . dataValue
+
+
+      toNotification'   :: IsFileOrDirectory t => t m (FileData FileLabel) -> Notification
+      toNotification' x = toNotification (extractData $ dataValue x) (rp </> name x)
+
+      ps </> n = Path u $ reverse (n : ps)
+
+
+-- | Merge two sorted lists into one sorted list
+merge               :: Ord a => [a] -> [a] -> [a]
+merge []     ys     = ys
+merge xs     []     = xs
+merge (x:xs) (y:ys)
+  | x <= y          = x : merge xs       (y : ys)
+  | otherwise       = y : merge (x : xs) ys
