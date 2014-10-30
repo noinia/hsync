1a4dd413efde27cec70f9b948ecbf3b946b50925
Frank Staals <frank@Hina.local>
2014-10-30 22:04:59 +0100

    Widget to view a directory

--- hsync-server/src/HSync/Server/Handler/ViewTree.hs
+++ hsync-server/src/HSync/Server/Handler/ViewTree.hs
@@ -1,18 +1,22 @@
 {-# LANGUAGE LambdaCase #-}
-module HSync.Server.Handler.ViewTree where
+module HSync.Server.Handler.ViewTree( getViewTreeR
+                                    , getViewStateR
+                                    ) where
 
-import Data.Maybe
-import Data.Either
-import HSync.Common.TimedFSTree
-import HSync.Common.FSTree
-import HSync.Server.Import
+import           Data.Maybe
+import           Data.Either
+import           HSync.Common.TimedFSTree
+import           HSync.Common.FSTree
+import           HSync.Server.Import hiding (fileName)
 
-import HSync.Server.FileSystemState
-import HSync.Server.AcidState
-import HSync.Server.AcidSync(QueryFSState(..))
+-- import           Text.Blaze(ToMarkup(..))
 
-import HSync.Server.Handler.FileActions(getTreeOf)
-import HSync.Server.Handler.Auth(protectRead, protectWrite)
+import           HSync.Server.FileSystemState
+import           HSync.Server.AcidState
+import           HSync.Server.AcidSync(QueryFSState(..))
+
+import           HSync.Server.Handler.FileActions(getTreeOf)
+import           HSync.Server.Handler.Auth(protectRead, protectWrite)
 
 import qualified Data.Map as M
 
@@ -21,22 +25,45 @@ import qualified Data.Map as M
 -- | Get the MTimeTree directly from the files directory
 getViewTreeR   :: Path -> Handler Html
 getViewTreeR p = protectRead p "vierTree" $ getTreeOf p >>= \case
-    Nothing -> notFound
-    Just t  -> defaultLayout $ [whamlet| #{show t} |]
+    Nothing -> defaultLayout $ [whamlet| noUserMsg p |]
+    Just t  -> defaultLayout . displayDir ViewTreeR p . unTree $ t
 
+-- | Get the MTimeTree by reading the FSState.
 getViewStateR   :: Path -> Handler Html
 getViewStateR p = protectRead p "viewState" $ queryDirectory p >>= \case
     Left err        -> defaultLayout $ [whamlet| err |]
-    Right (Left f)  -> defaultLayout $ [whamlet| #{show f} |]
-    Right (Right d) -> defaultLayout $ [whamlet| #{show d} |]
+    Right (Left f)  -> defaultLayout $ displayFile f
+    Right (Right d) -> defaultLayout $ displayDir ViewStateR p d
 
--- type Directory' = Directory (Max DateTime) FileLabel
+
+--------------------------------------------------------------------------------
 
 -- | Query Acid state for the MTimeTree corresponding to the input path
 queryDirectory   :: Path -> Handler (Either Text (Either File' Directory'))
 queryDirectory p = selectPath <$> queryAcid QueryFSState
   where
-    selectPath = maybe (Left noUserMsg) selectDir . M.lookup (owner p) . unM
-    selectDir  = maybe (Left noDirMsg)  Right . findAt (subPath p) . unTree
-    noUserMsg  = "No such user: " <> unUI (owner p)
-    noDirMsg   = "No such file or directory: " <> showT (subPath p)
+    selectPath = maybe (Left $ noUserMsg p) selectDir . M.lookup (owner p) . unM
+    selectDir  = maybe (Left $ noDirMsg p)  Right . findAt (subPath p) . unTree
+
+noDirMsg   :: Path -> Text
+noDirMsg p = "No such file or directory: " <> showT (subPath p)
+
+noUserMsg   :: Path -> Text
+noUserMsg p = "No such user: " <> unUI (owner p)
+
+--------------------------------------------------------------------------------
+-- | Widgets
+
+displayFile :: Show m => File m (FileData FileLabel) -> Widget
+displayFile f = [whamlet| #{show f} |]
+
+displayDir         :: (Show m, Show a)
+                   => (Path -> Route HSyncServer) -- ^ Route constructor
+                   -> Path  -- ^ Path to the directory we are displaying
+                   -> Directory m a
+                   -> Widget
+displayDir r p dir = $(widgetFile "viewDirectory")
+  where
+    route   :: IsFileOrDirectory t => t x y -> Route HSyncServer
+    route x = r (p { subPath = subPath p ++ [name x]} )
+              -- append the name to the path
