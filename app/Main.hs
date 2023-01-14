7b51e0d77f1c2c0a8f9bc4f56d98e867e8278249
Frank Staals <frank@fstaals.net>
2023-01-14 17:23:22 +0100

    reading file trees

--- app/Main.hs
+++ app/Main.hs
@@ -1,51 +1,127 @@
 {-# LANGUAGE UndecidableInstances #-}
 {-# LANGUAGE DefaultSignatures #-}
+{-# LANGUAGE OverloadedStrings  #-}
 module Main where
 
 import qualified MyLib (someFunc)
 
 import           Control.Lens
+import qualified Data.List as List
 import qualified Data.Map as Map
 import           Data.These
+import           Data.Time.Clock
+import           Flat
+import           GHC.Generics
+import           System.Directory.OsPath
 import           System.OsPath
+
+
+import           System.OsPath.Posix (PosixString)
+import           System.OsPath.Windows (WindowsString)
 
 --------------------------------------------------------------------------------
 
 main :: IO ()
 main = do
-  putStrLn "Hello, Haskell!"
   MyLib.someFunc
 
 
 type FileName = OsString
 
+instance Flat OsString
+instance Flat PosixString
+instance Flat WindowsString
 
-data FileTreeRoot d a = Root { _rootPath :: !OsPath
-                             , _tree     :: !(FileTree d a)
+
+
+-- | A rooted file tree.
+data FileTreeRoot d f = Root { _rootPath :: !OsPath
+                             , _tree     :: !(FileTree d f)
                              }
-                      deriving (Show,Eq)
+                      deriving stock (Show,Eq,Generic)
+
+instance (Flat d, Flat f) => Flat (FileTreeRoot d f)
+
+rootPath :: Lens' (FileTreeRoot d f) OsPath
+rootPath = lens _rootPath (\(Root _ t) r -> Root r t)
+
+tree :: Lens (FileTreeRoot d f) (FileTreeRoot e b) (FileTree d f) (FileTree e b)
+tree = lens _tree (\(Root r _) t -> Root r t)
+
 
 -- | the parent already stores the name of this file
 data FileTree d f = File      !f
                   | Directory !d (DirectoryContent d f)
-                  deriving (Show,Eq)
+                  deriving (Show,Eq,Generic)
 
 type DirectoryContent d f = Map.Map FileName (FileTree d f)
 
+instance (Flat d, Flat f) => Flat (FileTree d f)
+
+
 --------------------------------------------------------------------------------
 
+-- | Read the file with last modification times.
+readFileTreeWithLastModified :: OsPath -> IO (FileTreeRoot UTCTime UTCTime)
+readFileTreeWithLastModified = readFileTreeWith getModificationTime getModificationTime
+
+-- | reads a directory into a rooted File tree
+readFileTree :: OsPath -> IO (FileTreeRoot () ())
+readFileTree = readFileTreeWith (const $ pure ()) (const $ pure ())
+
+-- | reads a directory into a rooted File tree
+readFileTreeWith          :: (OsPath -> IO d) -- ^ to compute the dir info
+                          -> (OsPath -> IO f) -- ^ to compute the file info
+                          -> OsPath -- root hatp h
+                          -> IO (FileTreeRoot d f)
+readFileTreeWith d f path = Root path <$> readFileTree' d f path
+
+-- | The function that does the actual work reading the fileTree
+readFileTree'           :: (OsPath -> IO d) -- ^ to compute the dir info
+                        -> (OsPath -> IO f) -- ^ to compute the file info
+                        -> OsPath -> IO (FileTree d f)
+readFileTree' getD getF = go
+  where
+    go path = do d <- getD path
+                 content <- listDirectory path
+                 (dirs,files) <- partitionM (doesDirectoryExist . rooted) content
+                 files' <- mapM (\n -> (\f -> (n,File f)) <$> getF (rooted n)) files
+                 dirs'  <- mapM (\n -> (\t -> (n,t))      <$> go   (rooted n)) dirs
+                 pure $ Directory d (Map.fromList $ files' <> dirs')
+      where
+        rooted = (path </>)
+
+-- | Monadic version of partition
+partitionM   :: Monad m => (a -> m Bool) -> [a] -> m ([a],[a])
+partitionM p = fmap ( bimap (map fst) (map fst)
+                    . List.partition snd)
+             . mapM (\x -> (x,) <$> p x)
+
+--------------------------------------------------------------------------------
+
+-- | Data type modelling differences.
 data Diff delta = NoDifference
                 | Difference delta
-            deriving stock (Show,Eq,Functor,Foldable,Traversable)
+            deriving stock (Show,Eq,Functor,Foldable,Traversable,Generic)
 
 -- deriving stock instance (Show (Delta t)) => Show (Diff t)
 -- deriving stock instance (Eq (Delta t))   => Eq (Diff t)
 
 
+-- | A data type capturing that 'a' has changed from and old version
+-- to a new version.
 data Changed a = Changed {_from :: !a , _to :: !a }
-  deriving (Show,Eq)
+  deriving stock (Show,Eq,Generic)
 
+instance Flat a => Flat (Changed a)
 
+from :: Lens' (Changed a) a
+from = lens _from (\(Changed _ t) f -> Changed f t)
+
+to :: Lens' (Changed a) a
+to = lens _to   (\(Changed f _) t -> Changed f t)
+
+-- | default implementation of diff
 defaultDiff                        :: (Eq a, Delta a ~ Changed a)
                                    => a -> a -> Diff (Delta a)
 defaultDiff old new  | old == new  = NoDifference
@@ -66,8 +142,10 @@ instance HasDiff () where
 instance HasDiff Int where
   type Delta Int = Changed Int
 
+instance HasDiff UTCTime where
+  type Delta UTCTime = Changed UTCTime
 
-instance HasDiff FileName -- use the default
+instance HasDiff FileName
 
 
 -- data PairChanged a b = FirstChanged (Diff a)
@@ -107,14 +185,19 @@ data Updated a = Added !a
                | Deleted !a
                | Updated !(Delta a)
 
-deriving stock instance (Show a, Show (Delta a)) => Show (Updated a)
-deriving stock instance (Eq a, Eq (Delta a))     => Eq (Updated a)
+deriving stock instance (Show a, Show (Delta a))       => Show (Updated a)
+deriving stock instance (Eq a, Eq (Delta a))           => Eq (Updated a)
+deriving stock instance (Generic a, Generic (Delta a)) => Generic (Updated a)
+
+instance (Generic a, Generic (Delta a), Flat a, Flat (Delta a)) => Flat (Updated a)
 
 data Updates delta a = Updates { _added   :: [a]
                                , _deleted :: [a]
                                , _updated :: [delta]
                                }
-                       deriving (Show,Eq)
+                       deriving (Show,Eq,Generic)
+
+instance (Flat delta, Flat a) => Flat (Updates delta a)
 
 added   :: Lens' (Updates delta a) [a]
 added   = lens _added   (\(Updates _ ds us) as -> Updates as ds us)
@@ -133,7 +216,7 @@ instance Monoid (Updates delta a) where
   mempty = Updates mempty mempty mempty
 
 
-
+-- | Comptues the diff between two ordered lists
 diffOrderedLists                               :: (Ord k, HasDiff v) => [(k,v)] -> [(k,v)]
                                                -> Updates (k,Delta v) (k,v)
 diffOrderedLists []     []                     = mempty
@@ -157,67 +240,84 @@ instance (Ord k, HasDiff v) => HasDiff (Map.Map k v) where
                    Updates [] [] [] -> NoDifference
                    us               -> Difference us
 
-
 --------------------------------------------------------------------------------
 
-data FileTreeChanged d f = FileChanged (Delta f)
-                         | DirectoryAttributesChanged (Delta d)
-                         | DirectoryContentChanged (Delta (DirectoryContent d f))
-                         | DirectoryChanged (Delta d) (Delta (DirectoryContent d f))
-                         -- ^ both the local attributes changed, as well as the content
-                         | FileBecameDirectory f (d, DirectoryContent d f)
-                         | DirectoryBecameFile (d, DirectoryContent d f) f
-
-deriving stock instance (Show d, Show f, Show (Delta d), Show (Delta f)) => Show (FileTreeChanged d f)
-deriving stock instance (Eq d, Eq f, Eq (Delta d), Eq (Delta f)) => Eq (FileTreeChanged d f)
-
-
-instance (HasDiff f, HasDiff d) => HasDiff (FileTree d f) where
-  type Delta (FileTree d f) = FileTreeChanged d f
-
-  diff (File l)            (File r)             = FileChanged <$> diff l r
-  diff (File l)            (Directory d cntsR)  = Difference $ FileBecameDirectory l (d,cntsR)
-  diff (Directory d cntsL) (File r)             = Difference $ DirectoryBecameFile (d,cntsL) r
-  diff (Directory d cntsL) (Directory d' cntsR) = case (diff d d', diff cntsL cntsR) of
-    (NoDifference, NoDifference)             -> NoDifference
-    (Difference deltaD, NoDifference)        -> Difference $ DirectoryAttributesChanged deltaD
-    (NoDifference, Difference deltaCnt)      -> Difference $ DirectoryContentChanged deltaCnt
-    (Difference deltaD, Difference deltaCnt) -> Difference $ DirectoryChanged deltaD deltaCnt
-
-
-data DirDelta cache = OutdatedCache (Delta cache)
-                    | ContentChanged (Delta cache) (Diff (DirectoryContent cache f))
-
-
--- data DirDelta d f = OutdatedAttribute (Delta d)
---                   | ContentChange (Delta (DirectoryContent d f))
---                   | BothChanged (Delta )
-
-
-
-
-
-
-data FileTreeWithCacheChanged cache f dirDelta =
+data FileTreeChanged d f dirDelta =
     FileChanged (Delta f)
-  -- | DirectoryAttributesChanged (Delta d)
-  -- | DirectoryContentChanged (Delta (DirectoryContent d f))
   | DirectoryChanged dirDelta
-    -- ^ both the local attributes changed, as well as the content
+    -- ^ The directory has changed, dirDelta represents the possible changes.
   | FileBecameDirectory f (d, DirectoryContent d f)
   | DirectoryBecameFile (d, DirectoryContent d f) f
 
+deriving stock instance (Show d, Show f, Show dirDelta, Show (Delta f)
+                        ) => Show (FileTreeChanged d f dirDelta)
+deriving stock instance (Eq d, Eq f, Eq dirDelta, Eq (Delta f)
+                        ) => Eq (FileTreeChanged d f dirDelta)
+deriving stock instance (Generic d, Generic f, Generic dirDelta, Generic (Delta f)
+                        ) => Generic (FileTreeChanged d f dirDelta)
+
+
+data DirDelta cache f = OutdatedCache  (Delta cache)
+                      | ContentChanged (Delta cache) (Delta (DirectoryContent cache f))
+
+instance (HasDiff f, HasDiff d) => HasDiff (FileTree (Cache d) f) where
+  type Delta (FileTree (Cache d) f) = FileTreeChanged (Cache d) f (DirDelta (Cache d) f)
+
+  -- diff :: FileTree cache f -> FileTree cache f -> Delta (FileTree cache f)
+  diff (File l)            (File r)             = FileChanged <$> diff l r
+  diff (File l)            (Directory d cntsR)  = Difference $ FileBecameDirectory l (d,cntsR)
+  diff (Directory d cntsL) (File r)             = Difference $ DirectoryBecameFile (d,cntsL) r
+  diff (Directory cacheL cntsL) (Directory cacheR cntsR) = case diff cacheL cacheR of
+    NoDifference          -> NoDifference
+    Difference deltaCache -> Difference . DirectoryChanged $ case diff cntsL cntsR of
+        NoDifference      -> OutdatedCache deltaCache
+        Difference deltaCnt -> ContentChanged deltaCache deltaCnt
+  -- note: in particular that we are never producing a "directoryContentChanged" event in this case.
 
 
 
-diffCached :: FileTree cache f -> FileTree cache f -> Delta (FileTree cache f)
-diffCached (File l)            (File r)             = FileChanged <$> diff l r
-diffCached (File l)            (Directory d cntsR)  = Difference $ FileBecameDirectory l (d,cntsR)
-diffCached (Directory d cntsL) (File r)             = Difference $ DirectoryBecameFile (d,cntsL) r
-diffCached (Directory cacheL cntsL) (Directory cacheR cntsR) = case diff cacheL cacheR of
-  NoDifference          -> NoDifference
-  Difference deltaCache -> case diff cntsL cntsR of
-      NoDifference      -> Difference $ DirectoryAttributesChanged deltaCache
-                           -- the cache is outdated
-      Difference deltaCnt -> Difference $ DirectoryChanged deltaCache deltaCnt
--- note: in particular that we are never producing a "directoryContentChanged" event in this case.
+
+
+--------------------------------------------------------------------------------
+
+instance HasDiff d => HasDiff (Identity d) where
+  type Delta (Identity d) = Delta d
+  diff (Identity l) (Identity r) = diff l r
+
+
+-- data DirWithAttrChange d = DirAttrChange (Delta d)
+
+
+
+-- data DirWithAttrChange =
+
+
+-- instance (HasDiff f, HasDiff d
+--          ) => HasDiff (FileTree d f (DirWithAttrChange)
+
+--                                            ) where
+--   type Delta (FileTree (Identity d) f) = FileTreeChanged (Identity d)
+--                                                          f
+--                                          (These (Diff d)
+--                                                          (Diff (DirectoryContent (Identity d) f))
+--                                                   )
+
+--   diff (File l)            (File r)             = FileChanged <$> diff l r
+--   diff (File l)            (Directory d cntsR)  = Difference $ FileBecameDirectory l (d,cntsR)
+--   diff (Directory d cntsL) (File r)             = Difference $ DirectoryBecameFile (d,cntsL) r
+--   diff (Directory d cntsL) (Directory d' cntsR) = case (diff d d', diff cntsL cntsR) of
+--     (NoDifference, NoDifference)             -> NoDifference
+--     (Difference deltaD, NoDifference)        -> Difference . DirectoryChanged $ This deltaD
+--     (NoDifference, Difference deltaCnt)      -> Difference . DirectoryChanged $ That deltaCnt
+--     (Difference deltaD, Difference deltaCnt) -> Difference . DirectoryChanged $ These deltaD deltaCnt
+
+
+
+newtype Cache d = Cache d deriving stock (Show,Eq,Ord,Generic)
+
+instance Flat d => Flat (Cache d)
+
+
+instance HasDiff d => HasDiff (Cache d) where
+  type Delta (Cache d) = Delta d
+  diff (Cache l) (Cache r) = diff l r
