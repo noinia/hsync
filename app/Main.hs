fe6c93c820a234f0bb06062c25753b66dd23eee3
Frank Staals <frank@fstaals.net>
2023-01-14 19:11:43 +0100

    further fiddling; not really sure the dirattrs thing works as I hope it does

--- app/Main.hs
+++ app/Main.hs
@@ -242,7 +242,26 @@ instance (Ord k, HasDiff v) => HasDiff (Map.Map k v) where
 
 --------------------------------------------------------------------------------
 
-data FileTreeChanged d f dirDelta =
+-- | directory attributes that supports local attributes as well as a cache.
+data DirAttrs cache d = DirAttrs !cache -- ^ cached attributes about the content
+                                 !d  -- ^ "local" attributes about this directory itself
+                      deriving stock (Show,Eq,Ord,Generic)
+
+data DirAttrsDiff cache d = CacheOutdated     !(Delta cache)
+                          | LocalAttrsChanged !(Delta d)
+                          | BothChanged       !(Delta cache) !(Delta d)
+
+deriving instance (Show cache, Show d, Show (Delta cache), Show (Delta d)
+                  ) => Show (DirAttrsDiff cache d)
+deriving instance (Eq cache, Eq d, Eq (Delta cache), Eq (Delta d)
+                  ) => Eq (DirAttrsDiff cache d)
+
+instance (HasDiff cache, HasDiff d) => HasDiff (DirAttrs cache d) where
+  type Delta (DirAttrs cache d) = DirAttrsDiff cache d
+  diff (DirAttrs oldCache oldD) (DirAttrs newCache newD) =
+    these CacheOutdated LocalAttrsChanged BothChanged <$> diff (oldCache,oldD) (newCache,newD)
+
+data FileTreeChanged dirDelta d f =
     FileChanged (Delta f)
   | DirectoryChanged dirDelta
     -- ^ The directory has changed, dirDelta represents the possible changes.
@@ -250,33 +269,55 @@ data FileTreeChanged d f dirDelta =
   | DirectoryBecameFile (d, DirectoryContent d f) f
 
 deriving stock instance (Show d, Show f, Show dirDelta, Show (Delta f)
-                        ) => Show (FileTreeChanged d f dirDelta)
+                        ) => Show (FileTreeChanged dirDelta d f)
 deriving stock instance (Eq d, Eq f, Eq dirDelta, Eq (Delta f)
-                        ) => Eq (FileTreeChanged d f dirDelta)
+                        ) => Eq (FileTreeChanged dirDelta d f)
 deriving stock instance (Generic d, Generic f, Generic dirDelta, Generic (Delta f)
-                        ) => Generic (FileTreeChanged d f dirDelta)
+                        ) => Generic (FileTreeChanged dirDelta d f)
+
+data DirDelta d f = OnlyLocal      (Delta d)
+                  | ContentChanged (Delta d) (Delta (DirectoryContent d f))
+
+-- deriving stock instance (Show f, Show d, Show (Delta d), Show (Delta (FileTree d f))
+--                         ) => Show (DirDelta d f)
+-- -- -- TODO: this sounds dangerous...
+
+-- deriving stock instance (Eq (Delta d), Eq f, Eq d, Eq (Delta (FileTree d f))
+--                         ) => Eq (DirDelta d f)
 
 
-data DirDelta cache f = OutdatedCache  (Delta cache)
-                      | ContentChanged (Delta cache) (Delta (DirectoryContent cache f))
+testDelta :: DirDelta Int Int
+testDelta = OnlyLocal (Changed 6 8)
 
-instance (HasDiff f, HasDiff d) => HasDiff (FileTree (Cache d) f) where
-  type Delta (FileTree (Cache d) f) = FileTreeChanged (Cache d) f (DirDelta (Cache d) f)
+
+instance (HasDiff f, HasDiff d, HasDiff cache
+         ) => HasDiff (FileTree (DirAttrs cache d) f) where
+  -- ^ note that this instance requires that the cache is somehow kept up to date, i.e.
+  -- we will not attempt to test the children unless the local cache is outdated as well.
+
+  type Delta (FileTree (DirAttrs cache d) f) =
+    FileTreeChanged (DirDelta (DirAttrs cache d) f) (DirAttrs cache d) f
 
   -- diff :: FileTree cache f -> FileTree cache f -> Delta (FileTree cache f)
   diff (File l)            (File r)             = FileChanged <$> diff l r
   diff (File l)            (Directory d cntsR)  = Difference $ FileBecameDirectory l (d,cntsR)
   diff (Directory d cntsL) (File r)             = Difference $ DirectoryBecameFile (d,cntsL) r
-  diff (Directory cacheL cntsL) (Directory cacheR cntsR) = case diff cacheL cacheR of
-    NoDifference          -> NoDifference
-    Difference deltaCache -> Difference . DirectoryChanged $ case diff cntsL cntsR of
-        NoDifference      -> OutdatedCache deltaCache
-        Difference deltaCnt -> ContentChanged deltaCache deltaCnt
-  -- note: in particular that we are never producing a "directoryContentChanged" event in this case.
-
-
-
-
+  diff (Directory l cntsL) (Directory r cntsR) = case diff l r of
+      NoDifference          -> NoDifference
+        -- if there is no difference between the dirattrs, that in particular means
+        -- the cached subtree values are the same. Therefore, there is no need to explicitly
+        -- test the content
+      Difference deltaAttrs -> Difference . DirectoryChanged $ case deltaAttrs of
+        LocalAttrsChanged _               -> OnlyLocal deltaAttrs
+          -- Same here, only the local attributes changed, but the cache is still the same.
+          -- therefore, there is still no need to explicitly test the content.
+        CacheOutdated _                   -> testContent
+        BothChanged _ _                   -> testContent
+        where
+          -- explicitly test changes with the children/content
+          testContent = case diff cntsL cntsR of
+            NoDifference        -> OnlyLocal deltaAttrs -- we really only have local changes
+            Difference deltaCnt -> ContentChanged deltaAttrs deltaCnt -- children also have changes
 
 --------------------------------------------------------------------------------
 
